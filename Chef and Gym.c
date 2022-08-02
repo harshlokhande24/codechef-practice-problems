7c11c7e7c1ebe77ab9e3540ed2ef9d2cecca218f
harshlokhande24 <105076775+harshlokhande24@users.noreply.github.com>
2022-08-02 20:39:38 +0530

    Add files via upload

---
+++ Chef and Gym.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int T,X,Y,Z;
+    scanf("%d",&T);
+    
+    for(int i=0;i<T;i++)
+    {
+        scanf("%d %d %d",&X,&Y,&Z);
+        if(Z>=X+Y){
+            printf("2\n");
+        }
+        else if(X<=Z && Z<Y){
+            printf("1\n");
+        }
+        else if(Z<X){
+            printf("0\n");
+        }
+    }
+	return 0;
+}
